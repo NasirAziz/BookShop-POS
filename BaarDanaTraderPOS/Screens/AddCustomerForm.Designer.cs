fd7a922f4a2a3673ae6d6ebe26fed113a77beb7b
Abdul haseeb <jasialnew1@gmail.com>
2021-08-05 11:40:38 +0500

    Add Customer Completed

--- BaarDanaTraderPOS/Screens/AddCustomerForm.Designer.cs
+++ BaarDanaTraderPOS/Screens/AddCustomerForm.Designer.cs
@@ -54,9 +54,10 @@ namespace BaarDanaTraderPOS.Screens
             this.btnCustomerAdd.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnCustomerAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.btnCustomerAdd.ForeColor = System.Drawing.Color.White;
-            this.btnCustomerAdd.Location = new System.Drawing.Point(284, 449);
+            this.btnCustomerAdd.Location = new System.Drawing.Point(189, 292);
+            this.btnCustomerAdd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCustomerAdd.Name = "btnCustomerAdd";
-            this.btnCustomerAdd.Size = new System.Drawing.Size(110, 69);
+            this.btnCustomerAdd.Size = new System.Drawing.Size(73, 45);
             this.btnCustomerAdd.TabIndex = 0;
             this.btnCustomerAdd.Text = "Add";
             this.btnCustomerAdd.UseVisualStyleBackColor = false;
@@ -67,20 +68,23 @@ namespace BaarDanaTraderPOS.Screens
             this.btnCustomerCancel.BackColor = System.Drawing.Color.White;
             this.btnCustomerCancel.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnCustomerCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.btnCustomerCancel.Location = new System.Drawing.Point(52, 449);
+            this.btnCustomerCancel.Location = new System.Drawing.Point(35, 292);
+            this.btnCustomerCancel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCustomerCancel.Name = "btnCustomerCancel";
-            this.btnCustomerCancel.Size = new System.Drawing.Size(110, 69);
+            this.btnCustomerCancel.Size = new System.Drawing.Size(73, 45);
             this.btnCustomerCancel.TabIndex = 1;
             this.btnCustomerCancel.Text = "Cancel";
             this.btnCustomerCancel.UseVisualStyleBackColor = false;
+            this.btnCustomerCancel.Click += new System.EventHandler(this.btnCustomerCancel_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(50, 169);
+            this.label2.Location = new System.Drawing.Point(33, 110);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(83, 25);
+            this.label2.Size = new System.Drawing.Size(58, 17);
             this.label2.TabIndex = 3;
             this.label2.Text = "Name: *";
             // 
@@ -88,9 +92,10 @@ namespace BaarDanaTraderPOS.Screens
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(50, 248);
+            this.label3.Location = new System.Drawing.Point(33, 161);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(117, 25);
+            this.label3.Size = new System.Drawing.Size(84, 17);
             this.label3.TabIndex = 4;
             this.label3.Text = "Phone No. *";
             // 
@@ -98,35 +103,39 @@ namespace BaarDanaTraderPOS.Screens
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(48, 329);
+            this.label4.Location = new System.Drawing.Point(32, 214);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(91, 25);
+            this.label4.Size = new System.Drawing.Size(64, 17);
             this.label4.TabIndex = 5;
             this.label4.Text = "Address:";
             // 
             // tbCustomerName
             // 
             this.tbCustomerName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbCustomerName.Location = new System.Drawing.Point(244, 169);
+            this.tbCustomerName.Location = new System.Drawing.Point(163, 110);
+            this.tbCustomerName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbCustomerName.Name = "tbCustomerName";
-            this.tbCustomerName.Size = new System.Drawing.Size(266, 30);
+            this.tbCustomerName.Size = new System.Drawing.Size(179, 23);
             this.tbCustomerName.TabIndex = 7;
             // 
             // tbCustomerPhone
             // 
             this.tbCustomerPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbCustomerPhone.Location = new System.Drawing.Point(244, 248);
+            this.tbCustomerPhone.Location = new System.Drawing.Point(163, 161);
+            this.tbCustomerPhone.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbCustomerPhone.Name = "tbCustomerPhone";
-            this.tbCustomerPhone.Size = new System.Drawing.Size(266, 30);
+            this.tbCustomerPhone.Size = new System.Drawing.Size(179, 23);
             this.tbCustomerPhone.TabIndex = 8;
             // 
             // tbCustomerAddress
             // 
             this.tbCustomerAddress.AcceptsTab = true;
             this.tbCustomerAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbCustomerAddress.Location = new System.Drawing.Point(244, 324);
+            this.tbCustomerAddress.Location = new System.Drawing.Point(163, 211);
+            this.tbCustomerAddress.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbCustomerAddress.Name = "tbCustomerAddress";
-            this.tbCustomerAddress.Size = new System.Drawing.Size(266, 30);
+            this.tbCustomerAddress.Size = new System.Drawing.Size(179, 23);
             this.tbCustomerAddress.TabIndex = 9;
             // 
             // dgvAddCustomers
@@ -136,11 +145,12 @@ namespace BaarDanaTraderPOS.Screens
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dgvAddCustomers.BackgroundColor = System.Drawing.SystemColors.Control;
             this.dgvAddCustomers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvAddCustomers.Location = new System.Drawing.Point(550, 145);
+            this.dgvAddCustomers.Location = new System.Drawing.Point(367, 94);
+            this.dgvAddCustomers.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dgvAddCustomers.Name = "dgvAddCustomers";
             this.dgvAddCustomers.RowHeadersWidth = 62;
             this.dgvAddCustomers.RowTemplate.Height = 28;
-            this.dgvAddCustomers.Size = new System.Drawing.Size(724, 502);
+            this.dgvAddCustomers.Size = new System.Drawing.Size(563, 326);
             this.dgvAddCustomers.TabIndex = 10;
             this.dgvAddCustomers.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvAddCustomers_CellDoubleClick);
             // 
@@ -150,9 +160,10 @@ namespace BaarDanaTraderPOS.Screens
             this.btnCustomerUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnCustomerUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.btnCustomerUpdate.ForeColor = System.Drawing.Color.Black;
-            this.btnCustomerUpdate.Location = new System.Drawing.Point(168, 449);
+            this.btnCustomerUpdate.Location = new System.Drawing.Point(112, 292);
+            this.btnCustomerUpdate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCustomerUpdate.Name = "btnCustomerUpdate";
-            this.btnCustomerUpdate.Size = new System.Drawing.Size(110, 69);
+            this.btnCustomerUpdate.Size = new System.Drawing.Size(73, 45);
             this.btnCustomerUpdate.TabIndex = 11;
             this.btnCustomerUpdate.Text = "Update";
             this.btnCustomerUpdate.UseVisualStyleBackColor = false;
@@ -164,9 +175,10 @@ namespace BaarDanaTraderPOS.Screens
             this.btnCustomerDelete.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnCustomerDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.btnCustomerDelete.ForeColor = System.Drawing.Color.White;
-            this.btnCustomerDelete.Location = new System.Drawing.Point(400, 449);
+            this.btnCustomerDelete.Location = new System.Drawing.Point(267, 292);
+            this.btnCustomerDelete.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCustomerDelete.Name = "btnCustomerDelete";
-            this.btnCustomerDelete.Size = new System.Drawing.Size(110, 69);
+            this.btnCustomerDelete.Size = new System.Drawing.Size(73, 45);
             this.btnCustomerDelete.TabIndex = 12;
             this.btnCustomerDelete.Text = "Delete";
             this.btnCustomerDelete.UseVisualStyleBackColor = false;
@@ -175,9 +187,10 @@ namespace BaarDanaTraderPOS.Screens
             // btnACSearch
             // 
             this.btnACSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnACSearch.Location = new System.Drawing.Point(1122, 85);
+            this.btnACSearch.Location = new System.Drawing.Point(828, 55);
+            this.btnACSearch.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnACSearch.Name = "btnACSearch";
-            this.btnACSearch.Size = new System.Drawing.Size(90, 35);
+            this.btnACSearch.Size = new System.Drawing.Size(60, 23);
             this.btnACSearch.TabIndex = 13;
             this.btnACSearch.Text = "Search";
             this.btnACSearch.UseVisualStyleBackColor = true;
@@ -188,9 +201,10 @@ namespace BaarDanaTraderPOS.Screens
             this.tbACSearch.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tbACSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbACSearch.Location = new System.Drawing.Point(764, 88);
+            this.tbACSearch.Location = new System.Drawing.Point(509, 57);
+            this.tbACSearch.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbACSearch.Name = "tbACSearch";
-            this.tbACSearch.Size = new System.Drawing.Size(343, 30);
+            this.tbACSearch.Size = new System.Drawing.Size(310, 23);
             this.tbACSearch.TabIndex = 15;
             this.tbACSearch.TextChanged += new System.EventHandler(this.tbACSearch_TextChanged);
             // 
@@ -200,35 +214,38 @@ namespace BaarDanaTraderPOS.Screens
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(594, 91);
+            this.label5.Location = new System.Drawing.Point(396, 59);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(164, 25);
+            this.label5.Size = new System.Drawing.Size(117, 17);
             this.label5.TabIndex = 14;
             this.label5.Text = "Search by Name:";
             // 
             // tbCustomerBalance
             // 
             this.tbCustomerBalance.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbCustomerBalance.Location = new System.Drawing.Point(244, 388);
+            this.tbCustomerBalance.Location = new System.Drawing.Point(163, 252);
+            this.tbCustomerBalance.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbCustomerBalance.Name = "tbCustomerBalance";
-            this.tbCustomerBalance.Size = new System.Drawing.Size(266, 30);
+            this.tbCustomerBalance.Size = new System.Drawing.Size(179, 23);
             this.tbCustomerBalance.TabIndex = 17;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(48, 391);
+            this.label6.Location = new System.Drawing.Point(32, 254);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(172, 25);
+            this.label6.Size = new System.Drawing.Size(123, 17);
             this.label6.TabIndex = 16;
             this.label6.Text = "Current Balance: *";
             // 
             // AddCustomerForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1302, 689);
+            this.ClientSize = new System.Drawing.Size(948, 448);
             this.Controls.Add(this.tbCustomerBalance);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.tbACSearch);
@@ -245,6 +262,7 @@ namespace BaarDanaTraderPOS.Screens
             this.Controls.Add(this.label2);
             this.Controls.Add(this.btnCustomerCancel);
             this.Controls.Add(this.btnCustomerAdd);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "AddCustomerForm";
             this.Text = "AddCustomerForm";
             this.Load += new System.EventHandler(this.AddCustomerForm_Load);
--- BaarDanaTraderPOS/Screens/AddCustomerForm.cs
+++ BaarDanaTraderPOS/Screens/AddCustomerForm.cs
@@ -12,11 +12,9 @@ namespace BaarDanaTraderPOS.Screens
 {
     public partial class AddCustomerForm : Form
     {
-       
-
-
         SqlConnection con = new SqlConnection();
-       
+        public int id, phno, balance;
+        public String name, address;
 
         public AddCustomerForm()
         {
@@ -107,45 +105,73 @@ namespace BaarDanaTraderPOS.Screens
         private void btnCustomerDelete_Click(object sender, EventArgs e)
         {
             String name = tbCustomerName.Text;
-            DeleteCustomer(name);
+            DeleteCustomer(id);
         }
 
-        private void DeleteCustomer(String name)
+        private void DeleteCustomer(int id)
         {
-            if (name.Length == 0)
-            {
-                MessageBox.Show("Please enter Name to Delete!");
-            }
-            else if (name.Length > 0)
-            {
+            
+            
                 SqlCommand cmd = new SqlCommand();
                 cmd.Connection = con;
-                cmd.CommandText = "Delete from Add_customer where Name=@name";
+                cmd.CommandText = "Delete from Add_customer where Customer_id=@id";
                 cmd.CommandType = CommandType.Text;
-                cmd.Parameters.AddWithValue("@name", name);
+                cmd.Parameters.AddWithValue("@id", id);
                 cmd.ExecuteNonQuery();
                 LoadCustomers();
-            }
+            
 
         }
 
         private void btnCustomerUpdate_Click(object sender, EventArgs e)
         {
+            try
+            {
+                name = tbCustomerName.Text;
+                phno = Convert.ToInt32(tbCustomerPhone.Text);
+                try
+                {
+                    balance = Convert.ToInt32(tbCustomerBalance.Text);
+                }
+                catch
+                {
+                    balance = 0;
+                }
+
+                address = tbCustomerAddress.Text;
+            }
+            catch
+            {
+                MessageBox.Show("Please fill the requiered field");
+            }
+            
+            UpdateCustomer(id, name, address, phno,balance);
+            
+        }
+
+        private void btnCustomerCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
 
         }
 
-        private void UpdateCustomer(int id, String name, String address, int phone)
+        private void UpdateCustomer(int id, String name, String address, int phone,int balance)
         {
 
          SqlCommand cmd = new SqlCommand();
          cmd.Connection = con;
-         cmd.CommandText = "UPDATE Add_customer SET Name=@name Phone_no=@phone Address=@address Balance=@balance WHERE Customer_id=@id";
+         cmd.CommandText = "UPDATE Add_customer SET Name=@name, Phone_no=@phone, Address=@address, Balance=@balance WHERE Customer_id=@id";
          cmd.CommandType = CommandType.Text;
          cmd.Parameters.AddWithValue("@name", name);
          cmd.Parameters.AddWithValue("@id", id);
          cmd.Parameters.AddWithValue("@address", address);
          cmd.Parameters.AddWithValue("@phone", phone);
-         cmd.ExecuteNonQuery();
+        cmd.Parameters.AddWithValue("@balance", balance);
+        int r=cmd.ExecuteNonQuery();
+        if (r > 0)
+            {
+                MessageBox.Show("Updated");
+            }
          LoadCustomers();
           
 
@@ -169,18 +195,26 @@ namespace BaarDanaTraderPOS.Screens
 
         private void dgvAddCustomers_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
         {
-            int id = Convert.ToInt32(dgvAddCustomers.CurrentRow.Cells[0].Value.ToString());
-            String name= dgvAddCustomers.CurrentRow.Cells[1].Value.ToString();
-            int phno = Convert.ToInt32(dgvAddCustomers.CurrentRow.Cells[2].Value.ToString());
-            String Address = dgvAddCustomers.CurrentRow.Cells[3].Value.ToString();
-            int balance = Convert.ToInt32(dgvAddCustomers.CurrentRow.Cells[4].Value.ToString());
-            tbCustomerName.Text = name;
-            tbCustomerPhone.Text = phno.ToString();
-            tbCustomerBalance.Text = balance.ToString();
-            tbCustomerAddress.Text = Address;
+            try
+            {
+                id = Convert.ToInt32(dgvAddCustomers.CurrentRow.Cells[0].Value.ToString());
+                name = dgvAddCustomers.CurrentRow.Cells[1].Value.ToString();
+                phno = Convert.ToInt32(dgvAddCustomers.CurrentRow.Cells[2].Value.ToString());
+                address = dgvAddCustomers.CurrentRow.Cells[3].Value.ToString();
+                balance = Convert.ToInt32(dgvAddCustomers.CurrentRow.Cells[4].Value.ToString());
+                tbCustomerName.Text = name;
+                tbCustomerPhone.Text = phno.ToString();
+                tbCustomerBalance.Text = balance.ToString();
+                tbCustomerAddress.Text = address;
+            }
+            catch
+            {
 
-                
+            }
+
+        }
+        
 
         }
     }
-}
+
