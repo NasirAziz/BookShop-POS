3bef4b34c0b3a24f04c651764d650ae5e3bfa4ce
Abdul haseeb <jasialnew1@gmail.com>
2021-08-06 12:24:44 +0500

    report

--- BaarDanaTraderPOS/Properties/Settings.Designer.cs
+++ BaarDanaTraderPOS/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace BaarDanaTraderPOS.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -20,6 +20,16 @@ namespace BaarDanaTraderPOS.Properties {
         public static Settings Default {
             get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.;Initial Catalog=BaarDanaTraders;Integrated Security=True")]
+        public string BaarDanaTradersConnectionString {
+            get {
+                return ((string)(this["BaarDanaTradersConnectionString"]));
             }
         }
     }
--- BaarDanaTraderPOS/Screens/CreateOrderForm.cs
+++ BaarDanaTraderPOS/Screens/CreateOrderForm.cs
@@ -308,7 +308,8 @@ namespace BaarDanaTraderPOS.Screens
 
         private void btn_Confirm_Click(object sender, EventArgs e)
         {
-            
+            InvoiceViewer a = new InvoiceViewer();
+            a.Show();
         }
 
         private void btnCOConfirm_Click(object sender, EventArgs e)
